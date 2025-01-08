aac128c703f466bbc76f9f311172c14bed3ab109
SteamBlizzard <dan.sabo@live.ca>
2025-01-07 23:49:01 -0700

    just a lil smth

---
+++ java/src/ALilSmth_v5/Towers/DefenseTower.java
@@ -0,0 +1,15 @@
+package ALilSmth_v5.Towers;
+
+import ALilSmth_v5.Tower;
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+public class DefenseTower extends Tower {
+    public DefenseTower(RobotController robot) throws GameActionException {
+        super(robot);
+    }
+
+    public void turn() throws GameActionException {
+        super.turn();
+    }
+}
---
+++ java/src/ALilSmth_v5/Units/Splasher.java
@@ -0,0 +1,83 @@
+package ALilSmth_v5.Units;
+
+import ALilSmth_v5.Unit;
+import battlecode.common.*;
+
+public class Splasher extends Unit {
+    public Splasher(RobotController robot) throws GameActionException {
+        super(robot);
+    }
+
+    MapLocation returnLoc = null;
+
+    public void turn() throws Exception {
+        if (shouldRefill()) {
+            if (returnLoc == null) {
+                returnLoc = rc.getLocation();
+            }
+            findNearestPaintTower();
+            if (nearestPaintTower != null){
+                rc.setIndicatorString("Exploring on paint!! Trying to refill at: " + nearestPaintTower);
+            } else {
+                rc.setIndicatorString("Can't find refill station");
+            }
+            if(!moveToNearestPaintTower()){
+                tryExploreOnPaint();
+            }
+        } else {
+            // check if enemy tower in range, attack
+            RobotInfo tower = inEnemyTowerRange(rc.senseNearbyRobots(-1, rc.getTeam().opponent()));
+            if (tower != null) {
+                for (Direction dir : directions) {
+                    MapLocation newLocation = rc.getLocation().add(dir);
+                    int dist = newLocation.distanceSquaredTo(tower.getLocation());
+                    if (dist > tower.type.actionRadiusSquared && rc.canMove(dir) && !isEnemyPaint(rc.senseMapInfo(newLocation).getPaint())) {
+                        rc.move(dir);
+                        break;
+                    }
+                }
+                tryAttack(tower.getLocation());
+                // incase we're too close
+                if (rc.getLocation().distanceSquaredTo(tower.getLocation()) <= tower.type.actionRadiusSquared) {
+                    fuzzyMove(dirTo(tower.getLocation()).opposite());
+                }
+                // otherwise explore
+            } else {
+                if (rc.isActionReady()) {
+                    for (RobotInfo robot : rc.senseNearbyRobots(rc.getType().actionRadiusSquared, rc.getTeam().opponent())) {
+                        MapLocation enemyLoc = robot.getLocation();
+                        Direction enemyDir = rc.getLocation().directionTo(enemyLoc);
+                        MapLocation targetLoc = rc.getLocation().add(enemyDir).add(enemyDir);
+                        if ((!rc.senseMapInfo(targetLoc).getPaint().isAlly() && tryAttack(targetLoc)) || (!rc.senseMapInfo(enemyLoc).getPaint().isAlly() && tryAttack(enemyLoc))) {
+                            break;
+                        }
+                    }
+                }
+
+                if (returnLoc != null) {
+                    if (rc.canSenseLocation(returnLoc)) {
+                        returnLoc = null;
+                    } else {
+                        bugNav(returnLoc);
+                    }
+                }
+
+                Direction enemyPaintDirection = getEnemyPaintDirection();
+                if (enemyPaintDirection != null) {
+                    if (isEnemyPaint(rc.senseMapInfo(rc.getLocation()).getPaint())) {
+                        fuzzyMove(enemyPaintDirection.opposite());
+                    }
+                    if (RIGHT) {
+                        fuzzyMove(enemyPaintDirection.rotateRight().rotateRight());
+                    } else {
+                        fuzzyMove(enemyPaintDirection.rotateLeft().rotateLeft());
+                    }
+                } else {
+                    bugNav(explorer.getExploreTarget());
+                }
+            }
+        }
+
+        markNearbyMapData();
+    }
+}
