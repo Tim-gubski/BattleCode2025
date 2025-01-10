73b8b34c5935e466ca44c97fc511801688b5d323
Timur Gubskiy <34976482+Tim-gubski@users.noreply.github.com>
2025-01-09 21:31:25 -0700

    money

--- java/src/MoneyMan_v9/Helpers/MapData.java
+++ java/src/MoneyMan_v9/Helpers/MapData.java
@@ -1,18 +1,103 @@
 package MoneyMan_v9.Helpers;
 
-import battlecode.common.MapLocation;
+import battlecode.common.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class MapData {
-    public boolean[][] mapData;
+    public boolean[][] visited;
+    public Set<MapLocation> ruins = new HashSet<MapLocation>();
+    public Set<RobotInfo> friendlyTowers = new HashSet<RobotInfo>();
+
 
     public MapLocation[] paintTowers;
     public short towerIndex = 0;
 
     public MapData(int width, int height) {
-        mapData = new boolean[width][height];
+        visited = new boolean[width][height];
         paintTowers = new MapLocation[3]; // arbitrary
     }
 
+
+    int[] nearbyFriendlyTowersID = new int[10];
+    int numNearbyFriendlyTowers = 0;
+    public void updateLandmarks(MapLocation[] ruins, RobotInfo[] allies, MapLocation currentLocation){
+        // add new stuff
+        markRuins(ruins);
+
+        numNearbyFriendlyTowers = 0;
+        for(RobotInfo ally : allies){
+            if(ally.type.isTowerType()){
+                markFriendlyTower(ally);
+                nearbyFriendlyTowersID[numNearbyFriendlyTowers++] = ally.ID;
+            }
+        }
+
+        // remove stuff thats gone
+        for(RobotInfo previousTower : friendlyTowers){
+            if(currentLocation.distanceSquaredTo(previousTower.location) < GameConstants.VISION_RADIUS_SQUARED){
+                boolean stillThere = false;
+                for(int i = 0; i < numNearbyFriendlyTowers; i++){
+                    if(previousTower.ID == nearbyFriendlyTowersID[i]){
+                        stillThere = true;
+                        break;
+                    }
+                }
+                if(!stillThere){
+                    removeFriendlyTower(previousTower);
+                }
+            }
+        }
+    }
+
+    // RUIN STUFF
+    public void markRuins(MapLocation[] loc) {
+        for (MapLocation l : loc) {
+            ruins.add(l);
+        }
+    }
+
+    public void markRuin(MapLocation loc) {
+        ruins.add(loc);
+    }
+
+    public boolean isRuins(MapLocation loc) {
+        return ruins.contains(loc);
+    }
+
+
+    // FRIENDLY TOWER STUFF
+    public void markFriendlyTower(RobotInfo tower) {
+        friendlyTowers.add(tower);
+    }
+
+    public void markFriendlyTowers(RobotInfo[] towers) {
+        for (RobotInfo tower : towers) {
+            friendlyTowers.add(tower);
+        }
+    }
+
+    public void removeFriendlyTower(RobotInfo tower) {
+        friendlyTowers.remove(tower);
+    }
+
+    public RobotInfo[] getFriendlyTowers(){
+        return friendlyTowers.toArray(new RobotInfo[friendlyTowers.size()]);
+    }
+
+    public MapLocation[] getPaintTowers(){
+        MapLocation[] paintTowers = new MapLocation[friendlyTowers.size()];
+        int towerIndex = 0;
+        for(RobotInfo tower : friendlyTowers){
+            if(tower.type.getBaseType() == UnitType.LEVEL_ONE_PAINT_TOWER){
+                paintTowers[towerIndex++] = tower.location;
+            }
+        }
+        return paintTowers;
+    }
+
+    // OTHER STUFF
     public void setPaintTower(MapLocation towerLoc, MapLocation currLoc) {
         if (towerIndex >= paintTowers.length) {
             int furthestDist = currLoc.distanceSquaredTo(towerLoc);
@@ -69,15 +154,15 @@ public class MapData {
     }
 
     public boolean getVisited(int x, int y) {
-        return mapData[x][y];
+        return visited[x][y];
     }
 
     public void setVisited(int x, int y) {
-        mapData[x][y] = true;
+        visited[x][y] = true;
     }
 
     public void setVisited(MapLocation loc) {
-        mapData[loc.x][loc.y] = true;
+        visited[loc.x][loc.y] = true;
     }
 
 }
