73cc36c16394c77d8dee94e29fd753a625a34b31
SteamBlizzard <dan.sabo@live.ca>
2025-01-12 20:39:04 -0700

    harcoded rush bot for testing

---
+++ java/src/v13_Nyoom/Towers/DefenseTower.java
@@ -0,0 +1,15 @@
+package v13_Nyoom.Towers;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+import v13_Nyoom.Tower;
+
+public class DefenseTower extends Tower {
+    public DefenseTower(RobotController robot) throws GameActionException {
+        super(robot);
+    }
+
+    public void turn() throws GameActionException {
+        super.turn();
+    }
+}
